c0ca0c147ff70161590bd5e98d5382fcccc50866
Brennan Castillo <garthcast95@gmail.com>
2024-03-26 19:49:51 -0400

    Day 3 Commits

---
+++ Day 3 Projects/mainDay3Projects.py
@@ -0,0 +1,7 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height > 120:
+    print("Welcome aboard the rollercoaster!")
+else:
+    print("Sorry! You can't ride!")
