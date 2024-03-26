1e0384771ee6eeab6aea5fc3833664f5cad165c8
Brennan Castillo <garthcast95@gmail.com>
2024-03-26 15:44:14 -0400

    F string manipulation

---
+++ Day 2 Projects/mainDay2NumberManipulation.py
@@ -0,0 +1,10 @@
+#Round Function
+#comma 2 adds the number of decimal points
+print(round(8/3,2))
+
+#F String Manipulation. Added to print statement. calls all variables in f.
+score = 0
+height = 1.8
+isWinning = True
+
+print(f"your score is {score}, your height is {height}. You are Winning is {isWinning}")
