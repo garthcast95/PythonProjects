e39f134ae699ac4bf8658d38fccfe0bcc8f63da8
Brennan Castillo <garthcast95@gmail.com>
2024-03-26 15:21:32 -0400

    Two Digit Calculation

--- Day 2 Projects/mainDay2TypeChecking.py
+++ Day 2 Projects/mainDay2TypeChecking.py
@@ -4,10 +4,17 @@ charName = len(input("What is your name? \n"))
 #prints Value Type
 print (type(charName))
 
-#This will give an error since its an int value
+#This will give an error since its an int value which is length
 #print("Your Name has " + charName + " characters.")
 
 #Type Conversions
 
 newCharName = str(charName)
 print("Your Name has " + newCharName + " characters.")
+
+two_digit_number = input()
+type(two_digit_number)
+fdigitnumber = int(two_digit_number[0])
+sdigitnumber = int(two_digit_number[1])
+results = fdigitnumber + sdigitnumber
+print(results)
