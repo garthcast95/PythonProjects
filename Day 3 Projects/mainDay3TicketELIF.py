55c03acba41948626ae6fe9cf6f59ab2f028cd71
Brennan Castillo <garthcast95@gmail.com>
2024-03-26 21:05:33 -0400

    Ticket Bill

---
+++ Day 3 Projects/mainDay3TicketELIF.py
@@ -0,0 +1,23 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height >= 120:
+    print("Welcome aboard the rollercoaster!")
+    age = int(input("How old are you?\n"))
+    if age >= 18:
+        bill = 10
+        print("Please pay $10 for the ticket")
+    elif age <=12:
+        bill = 6
+        print("Please pay $6 for the ticket")    
+    else:
+        bill = 8
+        print("Please pay $8 for the ticket")    
+    askPhoto = input("Do you want a photo to be added? Y or N \n")
+    if askPhoto == "Y":
+        bill += 3
+        print(f"Your total will be ${bill}")
+    else:   
+        print(f"Your total will be ${bill}")
+else:
+    print("Sorry! You can't ride!")
