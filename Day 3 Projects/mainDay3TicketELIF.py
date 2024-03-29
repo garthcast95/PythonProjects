ee161e1c905f0735f42cd4850364cbe07edfd63c
Brennan Castillo <garthcast95@gmail.com>
2024-03-29 19:51:50 -0400

    Logic operators added

--- Day 3 Projects/mainDay3TicketELIF.py
+++ Day 3 Projects/mainDay3TicketELIF.py
@@ -4,15 +4,17 @@ height = int(input("What is your height in cm? "))
 if height >= 120:
     print("Welcome aboard the rollercoaster!")
     age = int(input("How old are you?\n"))
-    if age >= 18:
-        bill = 10
-        print("Please pay $10 for the ticket")
+    if  age <= 18:
+        bill = 8
+        print("Please pay $8 for the ticket")     
     elif age <=12:
         bill = 6
-        print("Please pay $6 for the ticket")    
+        print("Please pay $6 for the ticket")  
+    elif age >= 45 and age <= 50:
+        print("Your Ticket is on us!")      
     else:
-        bill = 8
-        print("Please pay $8 for the ticket")    
+        bill = 10
+        print("Please pay $10 for the ticket")   
     askPhoto = input("Do you want a photo to be added? Y or N \n")
     if askPhoto == "Y":
         bill += 3
